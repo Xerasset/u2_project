4d7a2cf54ed2b9c00a61a874b3e3dc80e7f80138
Tess Thompson <xerasset@gmail.com>
2022-06-26 11:05:18 -0400

    add vars to script.js to select elements needed

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 /**
  *
- * Lat + Long for cities in project
+ * Latitude + Longitude for cities in project as integers
  */
 
-const columbusLong = 39.961
-const columbusLat = 82.9988
+const columbusLong = 39.983334
+const columbusLat = -82.98333
 
-const tokyoLong = 35.6762
-const tokyoLat = 139.6503
+const tokyoLong = 35.652832
+const tokyoLat = 139.839478
 
-const viennaLong = 48.2082
-const viennaLat = 16.3738
+const viennaLong = 48.210033
+const viennaLat = 16.363449
 
-const sfLong = 37.7749
-const sfLAt = 122.4194
+const sfLong = 37.773972
+const sfLAt = -122.431297
 
-const milanLong = 45.4642
-const milanLat = 9.19
+const milanLong = 45.464664
+const milanLat = 9.18854
+
+/**
+ *
+ * Const vars for elements on in index.html + finalPage.html
+ */
+
+const index = 'index.html'
+const finalPage = 'finalPage.html'
+
+const sunriseButton = document.getElementById('sunrise-btn')
+const sunsetButton = document.getElementById('sunset-btn')
+// console.log(sunriseButton)
+// console.log(sunsetButton)
+
+/**
+ * Vars for date value on index.html
+ */
+const date = document.getElementById('datePicked')
+const finalPickedDate = date.value
+
+/**
+ * Vars to be populated on finalPage.html
+ */
+const finalPageH1 = document.getElementById('title')
+const finalPageP = document.getElementById('content')
