ce00beef188243470048860c9297e5bfb0dd06b1
Tess Thompson <xerasset@gmail.com>
2022-06-26 09:53:22 -0400

    Update script.js file ith lat + long of all locations + add note describing wheat vconst vars are for

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * Lat + Long for cities in project
+ */
+
+const columbusLong = 39.961
+const columbusLat = 82.9988
+
+const tokyoLong = 35.6762
+const tokyoLat = 139.6503
+
+const viennaLong = 48.2082
+const viennaLat = 16.3738
+
+const sfLong = 37.7749
+const sfLAt = 122.4194
+
+const milanLong = 45.4642
+const milanLat = 9.19
